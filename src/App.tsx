ada508975c2a77eb4cfe25f5b3726a37264c7614
nuurciye <44947339+nuurciye@users.noreply.github.com>
2026-03-16 00:31:54 +0300

    Add Services component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Services from './Services';
 
 function App() {
   return (
     <main className="bg-[#0B0B0B] min-h-screen">
       <Navbar />
       <Hero />
+      <Services />
     </main>
   );
 }
