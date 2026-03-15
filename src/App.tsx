d73fe27996f6d69c5283251cadcfe942711d1110
nuurciye <44947339+nuurciye@users.noreply.github.com>
2026-03-16 00:28:36 +0300

    Add Navbar component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
+import Navbar from './Navbar';
 import Hero from './Hero';
 
 function App() {
   return (
     <main className="bg-[#0B0B0B] min-h-screen">
-      {/* This displays the Hero component we just created */}
+      <Navbar />
       <Hero />
-      
-      {/* You can add your other sections here later */}
     </main>
   );
 }
 
 export default App;
-
