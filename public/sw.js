29564413fa732332a03e781a98ec6315efb7dc21
nuurciye <44947339+nuurciye@users.noreply.github.com>
2026-03-23 11:37:47 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,13 +2,28 @@ const CACHE_NAME = 'morla-barbershop-v1';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  'https://i.postimg.cc/VkTtR5yn/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
